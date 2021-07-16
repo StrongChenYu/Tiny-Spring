298a121ecf22498efdd56931165ee96e8808c711
chenyu <1084966084@qq.com>
2021-07-16 21:18:32 +0800

    feat(IOC): add beanDefinition and beanFactory

---
+++ src/main/java/com/csu/springframework/BeanDefinition.java
@@ -0,0 +1,13 @@
+package com.csu.springframework;
+
+public class BeanDefinition {
+
+    private Object bean;
+    public BeanDefinition(Object bean) {
+        this.bean = bean;
+    }
+
+    public Object getBean() {
+        return bean;
+    }
+}
